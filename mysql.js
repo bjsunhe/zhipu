a45c522700221d2a03b051a4458e29419815d5b5
bjsunhe <bjsunhe@sina.com>
2023-09-22 11:39:00 +0800

    Create mysql.js

---
+++ mysql.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'bmg666666',
+  database: 'bosch'
+});
+
+// Connect to MySQL
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+// Perform database operations here
+
+// Close the MySQL connection when done
+connection.end(err => {
+  if (err) {
+    console.error('Error closing MySQL connection:', err);
+  }
+  console.log('MySQL connection closed');
+});
