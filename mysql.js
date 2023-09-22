96a11fb1d6a695a1f1762fb544e4e8476fde15be
bjsunhe <bjsunhe@sina.com>
2023-09-22 11:43:00 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'bmg666666',
+  password: 'Cool1234567890-',
   database: 'bosch'
 });
 
