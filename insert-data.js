a605e1bf1cd3686d44a9a9165c058e6d411b9f06
bjsunhe <bjsunhe@sina.com>
2023-09-22 13:12:28 +0800

    Update insert-data.js

--- insert-data.js
+++ insert-data.js
@@ -27,11 +27,225 @@ connection.connect(err => {
     description: 'Description of the data'
   };
 
+
+  const newDatas=[
+{
+    process: 'screwing',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'screwing',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'screwing',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'forming',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'forming',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'forming',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'welding',
+    projectId: 'M6000676.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  },
+    {
+    process: 'dispensing',
+    projectId: 'M6000670.040',
+    sap: '0804CL0113',
+    productSizeLength: 45,
+    productSizeWidth: 20,
+    productSizeHeight: 30,
+    description: 'Description of the data'
+  }
+    
+    
+  ]
   // SQL query to insert data into the table
+  newDatas.forEach(item=>{
+  
   const insertQuery = 'INSERT INTO table_bosch SET ?';
 
   // Execute the insert query
-  connection.query(insertQuery, newData, (err, results) => {
+  connection.query(insertQuery, item, (err, results) => {
     if (err) {
       console.error('Error inserting data:', err);
     } else {
@@ -41,4 +255,6 @@ connection.connect(err => {
     // Close the MySQL connection
     connection.end();
   });
+  
+  })
 });
