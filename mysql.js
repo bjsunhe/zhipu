beec7d7d89dd13621ef18d422e4e46abb3afd9f0
bjsunhe <bjsunhe@sina.com>
2023-09-22 11:58:07 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -15,14 +15,40 @@ connection.connect(err => {
     return;
   }
   console.log('Connected to MySQL');
+
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS table_bosch (
+      process VARCHAR,
+      projectId VARCHAR,
+      sap VARCHAR,
+      productSizeLength INT,
+      productSizeWidth INT,
+      productSizeHeight INT,
+      description VARCHAR
+      
+    )
+  `;
+
+  // Execute the create table query
+  connection.query(createTableQuery, (err, results) => {
+    if (err) {
+      console.error('Error creating table:', err);
+    } else {
+      console.log('Table created successfully');
+    }
+
+    // Close the MySQL connection
+    connection.end();
+  });
+  
 });
 
 // Perform database operations here
 
 // Close the MySQL connection when done
-connection.end(err => {
-  if (err) {
-    console.error('Error closing MySQL connection:', err);
-  }
-  console.log('MySQL connection closed');
-});
+// connection.end(err => {
+//   if (err) {
+//     console.error('Error closing MySQL connection:', err);
+//   }
+//   console.log('MySQL connection closed');
+// });
